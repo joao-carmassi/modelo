c0e39df2a6c2051b26091e64ddf99232b99e0fe8
João Vitor Carmassi <90992816+joao-carmassi@users.noreply.github.com>
2024-12-04 16:57:03 -0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,31 @@
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import pluginVue from "eslint-plugin-vue";
+import vueTsEslintConfig from "@vue/eslint-config-typescript";
 
 export default [
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    name: "app/files-to-lint",
+    files: ["**/*.{ts,mts,tsx,vue}"],
   },
 
   {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    name: "app/files-to-ignore",
+    ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
   },
 
-  ...pluginVue.configs['flat/essential'],
+  ...pluginVue.configs["flat/essential"],
   ...vueTsEslintConfig(),
-]
+  {
+    rules: {
+      "@typescript-eslint/ban-ts-comment": [
+        "error",
+        {
+          "ts-ignore": true,
+          "ts-nocheck": {
+            description:
+              "necessário para ignorar erros em arquivos específicos",
+          },
+        },
+      ],
+    },
+  },
+];
